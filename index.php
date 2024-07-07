d63ef1539c95d4faf3ba89c2f3774060810537ce
b9Mike <migu31.z.a@gmail.com>
2024-07-07 15:41:43 -0600

    feat: Se agregaron los script para alertas y css de bulma

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
     require_once "./config/app.php";
     require_once "./autoload.php";
+    require_once "./app/views/inc/session_star.php";
 
     if(isset($_GET['views']))
         $url = explode("/", $_GET['views']);
     else
         $url = ['login'];
-    
 
 ?>
 <!DOCTYPE html>
@@ -15,7 +15,7 @@
     <?php require_once "./app/views/inc/head.php"; ?>
 </head>
 <body>
-    
+
     <?php require_once "./app/views/inc/script.php"; ?>
 </body>
 </html>
