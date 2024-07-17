c185c22239b102af1f92d3b8ac5db0830f548f7b
b9Mike <migu31.z.a@gmail.com>
2024-07-17 15:12:38 -0600

    feat: se agregaron los metodos de crud

--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -58,4 +58,113 @@
                 return true;
 
         }
+
+        //metodo para guardar datos
+        protected function guardarDatos($tabla, $datos){
+            
+            $query = "INSERT INTO $tabla (";
+
+            $count = 0;
+
+            foreach($datos as $dato){
+
+                if($count >= 1)
+                    $query .= ",";
+                $query .= $dato["campo_nombre"];
+                
+                $count++;
+
+            }
+
+            $query .= ") VALUES(";
+
+            $count = 0;
+
+            foreach($datos as $dato){
+
+                if($count >= 1)
+                    $query .= ",";
+                $query .= $dato["campo_marcador"];
+                
+                $count++;
+
+            }
+
+            $query .= ")";
+
+            $sql = $this->conectar()->prepare($query);
+
+            foreach($datos as $dato){
+                $sql->bindParam($dato["campo_marcador"], $dato["campo_valor"]);
+            }
+
+            $sql->execute();
+
+            return $sql;
+        }
+
+        public function seleccionarDatos($tipo, $tabla, $campo, $id){
+
+            $tipo = $this->limpiarCadena($tipo);
+            $tabla = $this->limpiarCadena($tabla);
+            $campo = $this->limpiarCadena($campo);
+            $id = $this->limpiarCadena($id);
+
+            if($tipo == "Unico"){
+                
+                $sql = $this->conectar()->prepare("SELECT * FROM $tabla WHERE $campo = :ID");
+                $sql->bindParam(":ID", $id);
+
+            }elseif($tipo == "Normal"){
+
+                $sql = $this->conectar()->prepare("SELECT $campo FROM $tabla");
+
+            }
+
+            $sql->execute();
+
+            return $sql;
+
+        }
+
+        protected function actualizarDatos($tabla, $datos, $condicion){
+            
+            $query = "UPDATE $tabla SET";
+
+            $count = 0;
+
+            foreach($datos as $dato){
+
+                if($count >= 1)
+                    $query .= ",";
+                $query .= $dato["campo_nombre"]."=".$dato["campo_marcador"];
+                
+                $count++;
+
+            }
+
+            $query .= " WHERE ".$condicion['condicion_campo']." = ".$condicion['condicion_marcador'];
+
+
+            $sql = $this->conectar()->prepare($query);
+
+            foreach($datos as $dato){
+                $sql->bindParam($dato["campo_marcador"], $dato["campo_valor"]);
+            }
+            $sql->bindParam($condicion["condicion_marcador"], $condicion["condicion_valor"]);
+
+            $sql->execute();
+
+            return $sql;
+        }
+
+        protected function eliminarDatos($tabla, $campo, $id){
+            
+            $sql = $this->conectar()->prepare("DELETE FROM $tabla WHERE $campo = :ID");
+            $sql->bindParam(":ID", $id);
+
+            $sql->execute();
+
+            return $sql;
+        }
     }
